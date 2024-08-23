5fa7d03d28bde4081bc32b0ad89ba034ad923eb8
xuxunsheng <xunsheng.xu@everxyz.com>
2024-08-23 19:38:07 +0800

    feat: css

--- src/style/css/index.tsx
+++ src/style/css/index.tsx
@@ -1,17 +1,56 @@
 import React from "react";
 import "./index.css";
 
+const BoxShadowDemo = () => {
+  // 用法
+  // 阴影的位置、模糊程度、扩展程度和颜色
+
+  return (
+    <div>
+      {/* outer */}
+      <div className='boxShadow'>BoxShadow</div>
+      {/* inner */}
+      <div className='boxShadow2'>BoxShadow</div>
+      <div className='boxShadow3'>BoxShadow</div>
+      <div className='boxShadow4Wrap'>
+        <div className='boxShadow4'>BoxShadow</div>
+      </div>
+    </div>
+  );
+};
+
+const ResizeDemo = () => {
+  // inline 元素不生效
+  // block 元素 overflow: visible 不生效
+  return (
+    <div>
+      <div className='resize'>resize</div>
+    </div>
+  );
+};
+
 const SelectorDemo = () => {
+  // # .
+  // ~
+  // :not :has -----
+  //    .button:not(:first-child)
+  //    span:has(+input[data-required])
   return (
     <div className={"selector"}>
-      <label className={"formItem"}>
-        <span>姓名</span>
-        <input data-required />
-      </label>
-      <label className={"formItem"}>
-        <span>地址</span>
-        <input />
-      </label>
+      <div>
+        <label className={"formItem"}>
+          <span>姓名</span>
+          <input data-required />
+        </label>
+        <label className={"formItem"}>
+          <span>地址</span>
+          <input />
+        </label>
+      </div>
+      <div>
+        <input type='checkbox' className={"checkbox"} />
+        <p className='checkboxText'>Example element that you can show or hide element</p>
+      </div>
     </div>
   );
 };
@@ -30,11 +69,37 @@ const TextDemo = () => {
   );
 };
 
+const BackDropFilterDemo = () => {
+  return (
+    <div className='backDropFilter'>
+      <div className='backDropFilter-blur'>
+        <div>backDropFilter</div>
+      </div>
+    </div>
+  );
+};
+
+const ScrollDemo = () => {
+  return (
+    <div className='scroll'>
+      <div className='scroll-inner'>
+        <div className='scroll-item'>scroll 1</div>
+        <div className='scroll-item'>scroll 2</div>
+        <div className='scroll-item'>scroll 3</div>
+      </div>
+    </div>
+  );
+};
+
 export default function CssDemo() {
   return (
     <div className='bg'>
+      <BoxShadowDemo />
+      <ResizeDemo />
       <SelectorDemo />
       <TextDemo />
+      <BackDropFilterDemo />
+      <ScrollDemo />
     </div>
   );
 }
